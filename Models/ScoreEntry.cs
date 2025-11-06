8638a08823c48972f0c279a670192bd661700afd
williamellings <wille.ellings@hotmail.com>
2025-11-06 10:55:26 +0100

    ScoreEntry class,

---
+++ Models/ScoreEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrickBreaker.Models;
+
+
+public sealed class ScoreEntry
+{
+    public string Username { get; set; }
+    public int Score { get; set; }
+    public DateTimeOffset At { get; set; }
+
+    public ScoreEntry(string username, int score, DateTimeOffset at)
+    {
+        Username = username;
+        Score = score;
+        At = at;
+    }
+}
