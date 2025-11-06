f51662873ac26b03b9cebc05306c93fc1288be52
Milo Eriksson <milo.arvaa@gmail.com>
2025-11-06 19:44:17 +0100

    Refactor Leaderboard class structure and formatting
    Refactor Leaderboard class for improved readability.

--- Logic/Leaderboard.cs
+++ Logic/Leaderboard.cs
@@ -1,30 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BrickBreaker.Models;
+using BrickBreaker.Storage;
 
 namespace BrickBreaker.Logic
 {
-    public class Leaderboard
-    {
-        using BrickBreaker.Models;
-        using BrickBreaker.Storage;
-
-        namespace BrickBreaker.Logic;
-
     public sealed class Leaderboard
-
     {
         private readonly LeaderboardStore _store;
-        public Leaderboard(LeaderboardStore store) => _store = store;
+
+        public Leaderboard(LeaderboardStore store)
+        {
+            _store = store;
+        }
 
         public void Submit(ScoreEntry entry)
         {
             _store.Add(entry);
         }
 
-        // convenience overload
+        // Convenience overload
         public void Submit(string username, int score)
         {
             _store.Add(new ScoreEntry(username, score, DateTimeOffset.UtcNow));
@@ -33,24 +29,23 @@ namespace BrickBreaker.Logic
         public List<ScoreEntry> Top(int n)
         {
             return _store.ReadAll()
-                 .OrderByDescending(s => s.Score)
-                 .ThenBy(s => s.At)
-                 .Take(n)
-                 .ToList();
+                .OrderByDescending(s => s.Score)
+                .ThenBy(s => s.At)
+                .Take(n)
+                .ToList();
         }
 
         public ScoreEntry? BestFor(string username)
         {
             return _store.ReadAll()
-                 .Where(s => s.Username.Equals(username, StringComparison.OrdinalIgnoreCase))
-                 .OrderByDescending(s => s.Score)
-                 .ThenBy(s => s.At)
-                 .FirstOrDefault();
+                .Where(s => s.Username.Equals(username, StringComparison.OrdinalIgnoreCase))
+                .OrderByDescending(s => s.Score)
+                .ThenBy(s => s.At)
+                .FirstOrDefault();
         }
     }
 }
-        
-}
+
 
 
 
