3b9fcd40bf54c2f3973a6c410c93b45325c85433
Lana <virsen.sv@gmail.com>
2025-11-21 15:04:04 +0100

    Fix User.cs some more

--- BrickBreaker.Core/Models/User.cs
+++ BrickBreaker.Core/Models/User.cs
@@ -1,14 +1,23 @@
-﻿namespace BrickBreaker.Models;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace BrickBreaker.Models;
 
 public sealed class User //define the attributes the class has
 {
     public required string Username { get; set; }
     public required string Password { get; set; }
 
+    [SetsRequiredMembers] // attribute to indicate that the constructor sets all required members
     public User(string username, string password) //constructer for the class
     {
         Username = username;
         Password = password;
     }
-    public User() { }
+
+    [SetsRequiredMembers]
+    public User()
+    {
+        Username = string.Empty;
+        Password = string.Empty;
+    }
 }
