20d3117c2fc6dbc80607b7221fa92bd73918183c
Lana <virsen.sv@gmail.com>
2025-11-26 10:55:58 +0100

    Fix the hardcoded height for the line below which the falling power-ups disappear (for 4k monitors power-ups disappered too early)

--- BrickBreaker/Core/GameEngine.cs
+++ BrickBreaker/Core/GameEngine.cs
@@ -57,7 +57,7 @@ namespace BrickBreaker
             }
 
             // Update PowerUps
-            UpdatePowerUps(paddleX, paddleY);
+            UpdatePowerUps(paddleX, paddleY, playArea);
 
             // D. Check Level Completion
             if (Bricks.All(b => !b.IsVisible))
@@ -130,7 +130,7 @@ namespace BrickBreaker
             }
         }
 
-        private void UpdatePowerUps(double paddleX, int paddleY)
+        private void UpdatePowerUps(double paddleX, int paddleY, Rectangle playArea)
         {
             // Define the paddle hitbox using the CURRENT width
             Rectangle paddleRect = new Rectangle((int)paddleX, paddleY, CurrentPaddleWidth, 20);
@@ -147,7 +147,7 @@ namespace BrickBreaker
                     ActivatePowerUp(p.Type); // <--- This triggers the effect!
                     PowerUps.RemoveAt(i);    // Remove from screen
                 }
-                else if (p.Y > 1000) // Remove if it falls off screen
+                else if (p.Y > playArea.Bottom) // Remove only after leaving visible area
                 {
                     PowerUps.RemoveAt(i);
                 }
