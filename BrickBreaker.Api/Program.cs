85cdbb6c3a47cddbdb4de47f2e17e90190d6e05e
Lana <virsen.sv@gmail.com>
2025-12-05 17:10:12 +0100

    Fix NuGet package issues

--- BrickBreaker.Api/Program.cs
+++ BrickBreaker.Api/Program.cs
@@ -5,7 +5,8 @@ using BrickBreaker.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton(sp =>
 {
@@ -30,7 +31,8 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapPost("/register", async (RegisterRequest request, IAuthService auth) =>
