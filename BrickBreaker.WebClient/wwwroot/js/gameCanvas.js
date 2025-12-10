286219165c039d450d185010bc16b9e9445b7ee2
Lana <virsen.sv@gmail.com>
2025-12-11 00:30:08 +0100

    Fix the paddle changing color on the ball launch

--- BrickBreaker.WebClient/wwwroot/js/gameCanvas.js
+++ BrickBreaker.WebClient/wwwroot/js/gameCanvas.js
@@ -104,9 +104,9 @@ function drawBalls(balls) {
 
 function drawPaddle(paddle, overlay) {
   ctx.save();
-  const restingColor = overlay.ballReady ? "#2ec4ff" : "#3b9dff";
+  const baseColor = "#2ec4ff"; // keep a consistent teal regardless of launch state
   // Show a reddish flash while the extender power-up winds down
-  ctx.fillStyle = overlay.paddleBlinking ? "#ff5c7a" : restingColor;
+  ctx.fillStyle = overlay.paddleBlinking ? "#ff5c7a" : baseColor;
   ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
   ctx.strokeStyle = "#081427";
   ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
